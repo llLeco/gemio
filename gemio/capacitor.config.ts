7d8887cce4fe617bab0a3e87fc8aa6ead317b623
Leandro <leandrollemos712@gmail.com>
2024-08-01 20:25:39 -0300

    Initialize Ionic Angular project

---
+++ gemio/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gemio',
+  webDir: 'www'
+};
+
+export default config;
